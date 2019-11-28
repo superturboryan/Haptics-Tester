dde0c341329e7b412628962836dedfc4f5d6d7ce
Ryan Forsyth <r.forsyth@intellisports.net>
2019-11-27 19:24:00 -0500

    Add local storage manager

---
+++ Haptics Tester/LocalStorageManager.h
@@ -0,0 +1,17 @@
+//
+//  LocalStorageManager.h
+//  Haptics Tester
+//
+//  Created by Ryan Forsyth on 2019-11-27.
+//  Copyright © 2019 Ryan Forsyth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocalStorageManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Haptics Tester/LocalStorageManager.m
@@ -0,0 +1,13 @@
+//
+//  LocalStorageManager.m
+//  Haptics Tester
+//
+//  Created by Ryan Forsyth on 2019-11-27.
+//  Copyright © 2019 Ryan Forsyth. All rights reserved.
+//
+
+#import "LocalStorageManager.h"
+
+@implementation LocalStorageManager
+
+@end
